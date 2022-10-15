5596ca88cdeea5826ee86a920d14c710707acbe5
Canyon Atwood <canyonatwood@gmail.com>
2022-10-14 22:26:19 -0600

    reminder.js

---
+++ modules/reminder.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+// create a model for each student
+const reminderSchema = new mongoose.Schema({
+    
+        "clientId": {type:String},
+        "clientUsername":{type:String,require:true},
+        "clientPassword": {type:String,require:true},
+        "addDate": {type:String},
+        "phone": {type:String},
+        "email": {type:String},
+        "isDone": {type:Boolean}
+      
+})
+
+module.exports = mongoose.model('client', reminderSchema);
