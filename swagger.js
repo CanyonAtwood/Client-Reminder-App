22d4a37b1366406837d72dedf063edf9175d1a2a
Emily Chandler <emilychandler@Emilys-MacBook-Pro.local>
2022-10-25 22:10:27 -0600

    change swagger to prod url

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
       title: 'My API',
       description: 'Description',
     },
-    host: 'localhost:3000',
-    schemes: ['http'],
+    host: 'client-reminder.onrender.com',
+    schemes: ['https'],
   };
 
 
