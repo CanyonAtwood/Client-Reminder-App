6759abe7608830aef25d18f54c7c4a5344b81f07
Canyon Atwood <canyonatwood@gmail.com>
2022-10-25 21:47:44 -0600

    callback

--- server.js
+++ server.js
@@ -91,7 +91,10 @@ const GOOGLE_CLIENT_SECRET = process.env.OAUTH_SECRET;
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL:'https://client-reminder.onrender.com/auth/google/callback'
+
+    //"http://localhost:3000/auth/google/callback"
+    
   },
   function(accessToken, refreshToken, profile, done) {
       userProfile=profile;
