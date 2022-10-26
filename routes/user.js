6309c730143733d80b69f0f6dd7ca6d7d29d5e72
Emily Chandler <emilychandler@Emilys-MacBook-Pro.local>
2022-10-25 22:24:18 -0600

    change get users route

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const routes = require("express").Router();
 const controller = require('../controllers/user');
 
 // GET all students
-routes.get('/', controller.getUser);
+routes.get('/', controller.getUsers);
 
 // GET specific student
 routes.get('/:id', controller.getUser);
